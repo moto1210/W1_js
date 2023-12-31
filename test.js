26f6baa711df3b0d3838e2583ab62af9df9fd715
apple <c1200922@st.kanazawa-it.ac.jp>
2023-12-31 09:01:53 +0900

    14commit

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
-document.write("<p>JavaScriptファイルの読み込みテストです。出力されていれば成功</p>");
+// document.write("<p>JavaScriptファイルの読み込みテストです。出力されていれば成功</p>");
+
+//これは１行のコメント
+
+/*これは１行のコメント*/
+
+/*
+複数行のコメントは
+これを使用することで
+可能になります。
+*/
+
+let num = 17;
+
+console.log(num.toString(2));
